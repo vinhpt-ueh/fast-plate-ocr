839a01ed315abedd37197055f5f2de8bee97958e
vinhpt-ueh <vinhpt-ueh@github.com>
2025-03-25 18:27:01 +0700

    drop out 0.4

--- fast_plate_ocr/train/model/models.py
+++ fast_plate_ocr/train/model/models.py
@@ -72,7 +72,7 @@ def head(x, max_plate_slots: int, vocabulary_size: int):
     x = GlobalAveragePooling2D()(x)
     # dropout for more robust learning
     # x = Dense(256, activation='relu')(x)
-    x = Dropout(0.6)(x)
+    x = Dropout(0.4)(x)
     dense_outputs = [
         Activation(softmax)(Dense(units=vocabulary_size)(x)) for _ in range(max_plate_slots)
     ]
